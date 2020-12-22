b6eb110933313bf6a16a914b2bf97a3e8cdad511
Jason <jason.chay@yahoo.com>
2020-12-22 12:47:29 -0500

    Added search form

--- script.py
+++ script.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
+from riotwatcher import LolWatcher, ApiError
+
 app = Flask(__name__)
 
-@app.route('/')            # when 'blog' is entered in the url, the following int is taken in as the variable postID
+@app.route('/')
 def home():
    return 'Hello World'
 
 @app.route('/<summoner_name>')
 def lookup(summoner_name):
     return 'Showing data for %s' % summoner_name
+
+@app.route('/search', methods = ['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('/', summoner_name = user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('/', summoner_name = user))
 
 if __name__ == '__main__':
    #app.run()
