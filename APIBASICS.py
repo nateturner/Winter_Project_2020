1dc6d9bd0dac3ac7cc934df23995609d78e2576e
nateturner <38406017+nateturner@users.noreply.github.com>
2020-12-22 12:58:19 -0500

    Add files via upload

---
+++ APIBASICS.py
@@ -0,0 +1,16 @@
+from riotwatcher import LolWatcher, ApiError
+
+# golbal variables
+api_key = 'RGAPI-XXXX'
+watcher = LolWatcher(api_key)
+my_region = 'na1'
+
+me = watcher.summoner.by_name(my_region, 'Rad Radius')
+print("ACCOUNT DATA")
+for line in me:
+    print(str(line) + ': ' + str(me[line]))
+print()
+my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
+print("RANKED DATA")
+for line in my_ranked_stats[0]:
+    print(str(line) + ': ' + str(my_ranked_stats[0][line]))
